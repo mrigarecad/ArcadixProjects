9e1bf0fe1d88bda76091646c1045c9f7a2904428
mrigarecad <mriganka.goswami@arcadix.com>
2023-06-09 20:02:00 +0530

    pupilprofile

---
+++ Arcadix_Swift_Pupil/Arcadix_Swift_Pupil/Modules/PupilProfile/PupilProfile.swift
@@ -0,0 +1,20 @@
+//
+//  PupilProfile.swift
+//  Arcadix_Swift_Pupil
+//
+//  Created by admin on 09/06/23.
+//
+
+import SwiftUI
+
+struct PupilProfile: View {
+    var body: some View {
+        Text("PupilProfile")
+    }
+}
+
+struct PupilProfile_Previews: PreviewProvider {
+    static var previews: some View {
+        PupilProfile()
+    }
+}
---
+++ Arcadix_Swift_Pupil/Arcadix_Swift_Pupil/Modules/PupilProfile/PupilProfile_ModuleProcessor.swift
@@ -0,0 +1,8 @@
+//
+//  PupilProfile_ModuleProcessor.swift
+//  Arcadix_Swift_Pupil
+//
+//  Created by admin on 09/06/23.
+//
+
+import Foundation
